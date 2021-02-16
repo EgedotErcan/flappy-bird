2b59529eeaa685f484098cb8185e427137c72aa4
EgedotErcan <ege-ercan@hotmail.com>
2021-02-16 22:09:21 +0200

    Yorumlar eklendi

--- Assets/Scripts/BoruMove.cs
+++ Assets/Scripts/BoruMove.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//BURASI DEĞİŞTİRİLDİ
 public class BoruMove : MonoBehaviour
 {
     public float speed;
     private float time= 6f;
-
+//BURASI DEĞİŞTİRİLDİ
     private void Start()
     {
         Destroy(gameObject, time);
     }
+    //BURASI DEĞİŞTİRİLDİ
     void FixedUpdate()
     {
         transform.position += Vector3.left * speed * Time.deltaTime;
